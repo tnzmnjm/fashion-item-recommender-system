3d54025e30a65561f57b31f4629a9b0d37e29593
Tannaz M <tannaz.monajemi@gmail.com>
2023-04-12 17:16:58 +0100

    Applied Ruff on knn_streamlit.py

--- knn_streamlit.py
+++ knn_streamlit.py
@@ -1,7 +1,6 @@
 from os.path import join
 
 import streamlit as st
-import tensorflow as tf
 from PIL import Image
 from joblib import load
 import numpy as np
@@ -15,16 +14,19 @@ def substitution_product_recommendation(product_id):
     index_in_random_id = np.where(random_ids == product_id)[0][0]
     index_in_random_vectorised_matrix = index_in_random_id
     distance, nbr_indices_in_vectorised_matrix = \
-        clf.kneighbors(vectorised_matrix[index_in_random_vectorised_matrix].reshape(1, -1))
+        clf.kneighbors(vectorised_matrix[index_in_random_vectorised_matrix]
+                       .reshape(1, -1))
     nbrs_product_ids = []
     for i in range(len(nbr_indices_in_vectorised_matrix[0])):
-        nbrs_product_ids.append(random_ids[nbr_indices_in_vectorised_matrix[0][i]])
+        nbrs_product_ids.append\
+            (random_ids[nbr_indices_in_vectorised_matrix[0][i]])
     selected_df = df_random.loc[df_random.id.isin(nbrs_product_ids)]
 
     return selected_df
 
 
-df = pd.read_csv('/Users/tannazmnjm/Downloads/archive/styles.csv', sep=',',
+df = pd.read_csv('/Users/tannazmnjm/Downloads/archive/styles.csv',
+                 sep=',',
                  engine='python', on_bad_lines='skip')
 
 clf = load('knn_model.joblib')
@@ -41,8 +43,10 @@ st.title('Fashion items Dataset')
 st.text(f'Select from available products: {len(df_random)}')
 
 
-selected_product = st.selectbox(label='Please choose/search for a product :', options=df_random.productDisplayName.values)
-selected_id = df_random[df_random.productDisplayName == selected_product]["id"].values[0]
+selected_product = st.selectbox(label='Please choose/search for a product :',
+                                options=df_random.productDisplayName.values)
+selected_id = \
+    df_random[df_random.productDisplayName == selected_product]["id"].values[0]
 
 
 if selected_product is not None:
@@ -58,10 +62,11 @@ if selected_product is not None:
         # dropping the selected id from the result
         substitution_df = substitution_df[substitution_df["id"] != selected_id]
 
-        # recommended product will have the same gender unless it's unisex which will be everything
+        # recommended product will have the same gender unless it's unisex which
+        # will be everything
 
-        st.subheader(f'Substitutable Products')
-        st.subheader(f'Please choose your required filters')
+        st.subheader('Substitutable Products')
+        st.subheader('Please choose your required filters')
 
         use_gender = st.checkbox('Gender')
         use_subCategory = st.checkbox('Sub Category')
@@ -69,16 +74,20 @@ if selected_product is not None:
         use_baseColour = st.checkbox('Colour')
 
         if use_gender:
-            selected_id_gender = df_random.loc[df_random.id == selected_id, "gender"].values[0]
+            selected_id_gender = \
+                df_random.loc[df_random.id == selected_id, "gender"].values[0]
             if selected_id_gender != 'Unisex':
-                substitution_df = substitution_df[substitution_df.gender == selected_id_gender]
+                substitution_df = \
+                    substitution_df[substitution_df.gender == selected_id_gender]
 
         if use_subCategory:
-            subCategory = df_random.loc[df_random.id == selected_id, "subCategory"].values[0]
+            subCategory = \
+                df_random.loc[df_random.id == selected_id, "subCategory"].values[0]
             substitution_df = substitution_df[substitution_df.subCategory == subCategory]
 
         if use_articleType:
-            articleType = df_random.loc[df_random.id == selected_id , "articleType"].values[0]
+            articleType = \
+                df_random.loc[df_random.id == selected_id , "articleType"].values[0]
             substitution_df = substitution_df[substitution_df.articleType == articleType]
 
         if use_baseColour:
